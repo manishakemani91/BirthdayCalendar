7b2ab917db16a810c1d9b71d3dc5073fcdc5389c
manishakemani91 <manishakemani91@gmail.com>
2020-01-19 22:48:33 +0530

    updated app.js
    updated app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 function generatediv() {
     try {
-        var dataInput = isJSON(document.getElementById("jsonInput").value);
+        const dataInput = isJSON(document.getElementById("jsonInput").value);
         if (dataInput) {
 
             clearChildNodes();
-            var month = document.getElementById('numberInput').value;
-            var filteredResult = dataInput.filter(x => x.birthday.indexOf(month) > -1);
+            const month = document.getElementById('numberInput').value;
+            const filteredResult = dataInput.filter(x => x.birthday.indexOf(month) > -1);
 
             if (filteredResult && filteredResult.length > 0) {
 
-                var sortedResult = filteredResult.sort((a, b) => {
+                const sortedResult = filteredResult.sort((a, b) => {
                     a = new Date(a.birthday);
                     b = new Date(b.birthday);
                     return (b - a);
@@ -20,10 +20,10 @@ function generatediv() {
                     });
                 }
 
-                var childElement = document.getElementsByClassName("mainbody");
+                let childElement = document.getElementsByClassName("mainbody");
                 Array.prototype.forEach.call(childElement, function (el) {
                     if (!el.hasChildNodes()) {
-                        var item = document.createElement("div");
+                        let item = document.createElement("div");
                         item.setAttribute("data-tooltip", 'No Birthday');
                         item.setAttribute("class", "empty-node");
                         item.innerHTML = ':S';
@@ -44,7 +44,7 @@ function generatediv() {
 }
 
 function clearChildNodes() {
-    var childElement = document.getElementsByClassName("mainbody");
+    let childElement = document.getElementsByClassName("mainbody");
     Array.prototype.forEach.call(childElement, function (el) {
         el.innerHTML = '';
     });
@@ -53,12 +53,12 @@ function clearChildNodes() {
 function createChildNodes(element) {
     const monthNames = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
     const month = (new Date(element.birthday)).getMonth();
-    var ParentElement = document.getElementById(monthNames[month]);
-    var childElement = ParentElement.getElementsByClassName("mainbody")[0];
+    let ParentElement = document.getElementById(monthNames[month]);
+    let childElement = ParentElement.getElementsByClassName("mainbody")[0];
     childElement.style["grid-template-columns"] = "repeat(3,1fr)";
     const initials = element.name.split(" ").map((n) => n[0]).join("");
-    var info = initials;
-    var item = document.createElement("div");
+    const info = initials;
+    let item = document.createElement("div");
     item.setAttribute("data-tooltip", element.name);
     item.setAttribute("class", "child-item");
     item.innerHTML = info;
